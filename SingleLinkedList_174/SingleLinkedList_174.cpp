2289e4153c02a88ee64ef128398bdc36a8c464af
adandoang <akhdanjzif@gmail.com>
2023-05-19 16:08:54 +0700

    memperbaiki void

--- SingleLinkedList_174/SingleLinkedList_174.cpp
+++ SingleLinkedList_174/SingleLinkedList_174.cpp
@@ -26,9 +26,27 @@ void addNode() {
 			cout << "NIM sudah ada" << endl;
 			return;
 		}
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return;
 	}
+
+	Node* previous = START;
+	Node* current = START;
+
+	while ((current != NULL) && (nim >= current->noMhs))
+	{
+		if (nim == current->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+		previous = current;
+		current = current->next;
+	}
+
+	nodeBaru->next = current;
+	previous->next = nodeBaru;
 }
 
-Node* current = START;
-Node* prevous = START;
-
+	
