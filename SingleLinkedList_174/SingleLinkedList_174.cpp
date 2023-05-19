70274ce664f0674111b64ce5ed200d3a03cf0599
adandoang <akhdanjzif@gmail.com>
2023-05-19 11:05:53 +0700

    void addnode

--- SingleLinkedList_174/SingleLinkedList_174.cpp
+++ SingleLinkedList_174/SingleLinkedList_174.cpp
@@ -8,3 +8,23 @@ struct Node
 	Node* next;
 };
 
+Node* START = NULL;
+
+void addNode() {
+	int nim;
+	string nama;
+	Node* nodeBaru = new Node();
+	cout << "Masukan NIM: ";
+	cin >> nim;
+	cout << "Masukan nama: ";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
+
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs) {
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+	}
+}
